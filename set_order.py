cdb0b50bff2f279dd2a177b93c4ec01bbb735748
Ken Schultz <schultzy51@gmail.com>
2016-03-12 13:36:49 -0600

    Add additional columns to set

--- set_order.py
+++ set_order.py
@@ -13,7 +13,24 @@ BASE_URL = 'http://brickset.com'
 LOGIN_URL = BASE_URL + '/api/v2.asmx/login'
 GET_SETS_URL = BASE_URL + '/api/v2.asmx/getSets'
 OUTPUT_CSV = 'output.csv'
-CSV_HEADER = ['id', 'name', 'year', 'url', 'price', 'us start', 'us end', 'uk start', 'uk end']
+CSV_HEADER = ['id',
+              'number',
+              'number variant',
+              'name',
+              'year',
+              'theme',
+              'theme group',
+              'subtheme',
+              'pieces',
+              'minifigs',
+              'released',
+              'url',
+              'us retail price',
+              'last updated',
+              'us start',
+              'us end',
+              'uk start',
+              'uk end']
 
 
 class Config:
@@ -24,13 +41,53 @@ class Config:
 
 
 class Set:
-  def __init__(self, id, name, year, url, price):
+  def __init__(self, id, number, number_variant, name, year, theme, theme_group, subtheme, pieces, minifigs, released, url, us_retail_price, last_updated):
     self.id = id
+    self.number = number
+    self.number_variant = number_variant
     self.name = name
     self.year = year
+    self.theme = theme
+    self.theme_group = theme_group
+    self.subtheme = subtheme
+    self.pieces = pieces
+    self.minifigs = minifigs
+    self.released = released
     self.url = url  # brickset url
-    self.price = price  # us retail price
+    self.us_retail_price = us_retail_price  # us retail price
+    self.last_updated = last_updated
 
+  def __str__(self):
+    return "{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(self.id,
+                                                                           self.number,
+                                                                           self.number_variant,
+                                                                           self.name,
+                                                                           self.year,
+                                                                           self.theme,
+                                                                           self.theme_group,
+                                                                           self.subtheme,
+                                                                           self.pieces,
+                                                                           self.minifigs,
+                                                                           self.released,
+                                                                           self.url,
+                                                                           self.us_retail_price,
+                                                                           self.last_updated)
+
+  def to_a(self):
+    return [self.id,
+            self.number,
+            self.number_variant,
+            self.name,
+            self.year,
+            self.theme,
+            self.theme_group,
+            self.subtheme,
+            self.pieces,
+            self.minifigs,
+            self.released,
+            self.url,
+            self.us_retail_price,
+            self.last_updated]
 
 def get_config(file='.config'):
   with open(file, 'r') as f:
@@ -76,10 +133,19 @@ def get_sets(config, token):
 
   sets = [
     Set(s.setID.cdata.strip(),
+        s.number.cdata.strip(),
+        s.numberVariant.cdata.strip(),
         s.name.cdata.strip(),
         s.year.cdata.strip(),
+        s.theme.cdata.strip(),
+        s.themeGroup.cdata.strip(),
+        s.subtheme.cdata.strip(),
+        s.pieces.cdata.strip(),
+        s.minifigs.cdata.strip(),
+        s.released.cdata.strip(),
         s.bricksetURL.cdata.strip(),
-        s.USRetailPrice.cdata.strip())
+        s.USRetailPrice.cdata.strip(),
+        s.lastUpdated.cdata.strip())
     for s in doc.ArrayOfSets.sets
     ]
 
@@ -128,8 +194,8 @@ sets = get_sets(config, token)
 values = []
 
 for s in sets:
-  print "{}, {}, {}, {}, {}".format(s.id, s.name, s.year, s.url, s.price)
-  values.append([s.id, s.name, s.year, s.url, s.price] + get_dates(s.url))
+  print s.to_a()
+  values.append(s.to_a() + get_dates(s.url))
 
 us_ordered = sorted(values, key=itemgetter(5))
 uk_ordered = sorted(values, key=itemgetter(7))

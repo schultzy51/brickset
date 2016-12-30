1265a38218cf1c1b64d2edac5db7323349ae4704
Ken Schultz <schultzy51@gmail.com>
2016-12-30 13:31:45 -0600

    WIP

--- tools/brickset.py
+++ tools/brickset.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
 
 import argparse
-import zeep
 import simplejson as json
+import zeep
+from csv import DictWriter
 from datetime import datetime
+from io import StringIO
 from time import sleep
+from IPython import embed
 
 parser = argparse.ArgumentParser(description='Brickset Tooling')
-parser.add_argument('command', help='Command to execute')
+parser.add_argument('-c', '--command', action='store', dest='command', help='Command to execute')
 parser.add_argument('-a', '--api-key', action='store', dest='api_key', help='Developer api-key', required=True)
 parser.add_argument('-u', '--username', action='store', dest='username', help='Username')
 parser.add_argument('-p', '--password', action='store', dest='password', help='Password')
-parser.add_argument('-m', '--minutes-ago', action='store', dest='minutes_ago', type=int, default=10080,
-                    help='Recent minutes ago')
-parser.add_argument('-d', '--delay', action='store', dest='delay', type=int, default=2,
-                    help='Delay between api calls in seconds')
+parser.add_argument('-m', '--minutes-ago', action='store', dest='minutes_ago', type=int, default=10080, help='Recent minutes ago')
+parser.add_argument('-d', '--delay', action='store', dest='delay', type=int, default=2, help='Delay between api calls in seconds')
 parser.add_argument('-t', '--theme', action='store', dest='theme', help='Theme')
-# parser.add_argument('-o', '--output', action='store', dest='output', default='jsonl', help='Output format')
+parser.add_argument('-o', '--output', action='store', dest='output', default='jsonl', help='Output format')
+parser.add_argument('-f', '--fields', dest='fields', action='store', default=[], nargs='+', help='Fields to output')
 
 args = parser.parse_args()
+
+print(args)
+
+if args.output not in ['jsonl', 'csv']:
+    raise ValueError('invalid output format')
+
 client = zeep.Client('http://brickset.com/api/v2.asmx?WSDL')
 
 
@@ -49,41 +57,33 @@ def json_serial(obj):
     raise TypeError("Type not serializable")
 
 
+objs = []
+
 if args.command == 'recent':
     zeep_sets = client.service.getRecentlyUpdatedSets(apiKey=args.api_key, minutesAgo=args.minutes_ago)
-    sets = zeep.helpers.serialize_object(zeep_sets)
-
-    for set in sets:
-        print(json.dumps(set, default=json_serial))
+    objs = zeep.helpers.serialize_object(zeep_sets)
 
 elif args.command == 'wanted':
     token = client.service.login(apiKey=args.api_key, username=args.username, password=args.password)
     zeep_sets = client.service.getSets(**sets_params({'userHash': token, 'wanted': 1}))
-    sets = zeep.helpers.serialize_object(zeep_sets)
+    objs = zeep.helpers.serialize_object(zeep_sets)
 
-    for set in sets:
-        print(json.dumps(set, default=json_serial))
+    # Custom sort
+    objs.sort(key=lambda obj: obj['number'])
+    objs.sort(key=lambda obj: obj['released'], reverse=True)
+    objs.sort(key=lambda obj: (obj['USDateAddedToSAH'] is None, obj['USDateAddedToSAH']))
 
 elif args.command == 'themes':
     zeep_themes = client.service.getThemes(apiKey=args.api_key)
-    themes = zeep.helpers.serialize_object(zeep_themes)
-
-    for theme in themes:
-        print(json.dumps(theme, default=json_serial))
+    objs = zeep.helpers.serialize_object(zeep_themes)
 
 elif args.command == 'subthemes':
     zeep_subthemes = client.service.getSubthemes(apiKey=args.api_key, theme=args.theme)
-    subthemes = zeep.helpers.serialize_object(zeep_subthemes)
-
-    for subtheme in subthemes:
-        print(json.dumps(subtheme, default=json_serial))
+    objs = zeep.helpers.serialize_object(zeep_subthemes)
 
 elif args.command == 'years':
     zeep_years = client.service.getYears(apiKey=args.api_key, theme=args.theme)
-    years = zeep.helpers.serialize_object(zeep_years)
-
-    for year in years:
-        print(json.dumps(year, default=json_serial))
+    objs = zeep.helpers.serialize_object(zeep_years)
 
 elif args.command == 'sets':
     page_number = 1
@@ -94,8 +94,7 @@ elif args.command == 'sets':
         zeep_sets = client.service.getSets(**params)
         sets = zeep.helpers.serialize_object(zeep_sets)
 
-        for set in sets:
-            print(json.dumps(set, default=json_serial))
+        objs += sets
 
         if len(sets) != page_size:
             break
@@ -105,3 +104,24 @@ elif args.command == 'sets':
 
 else:
     print('Unknown Command')
+
+if args.fields:
+    for obj in objs:
+        obj = OrderedDict((k, obj[k]) for k in args.fields if k in obj)
+
+fp = StringIO()
+
+if args.output == 'csv':
+    # keys = ['setID', 'number', 'year', 'released', 'USDateAddedToSAH']
+    # writer = DictWriter(fp, fieldnames=keys, extrasaction='ignore')
+    keys = objs[0].keys()
+    writer = DictWriter(fp, fieldnames=keys, extrasaction='ignore')
+    writer.writeheader()
+    for obj in objs:
+        writer.writerow(obj)
+
+elif args.output == 'jsonl':
+    for obj in objs:
+        fp.write(json.dumps(obj, default=json_serial) + '\n')
+
+print(fp.getvalue())

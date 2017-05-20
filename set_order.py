4a5d7af3605c3691136d9edab8383b4a79e329ba
Ken Schultz <schultzy51@gmail.com>
2017-05-19 20:50:33 -0500

    Complete rewrite of set_order.py

--- set_order.py
+++ set_order.py
@@ -1,257 +1,85 @@
 #!/usr/bin/python
 
 import csv
+import logging
 import os
-import requests
-import time
-import untangle
+import zeep
 import ConfigParser
-from lxml import html
+from datetime import datetime
+from collections import OrderedDict
 
-BASE_URL = 'https://brickset.com'
-LOGIN_URL = BASE_URL + '/api/v2.asmx/login'
-GET_SETS_URL = BASE_URL + '/api/v2.asmx/getSets'
-OUTPUT_CSV = 'output.csv'
-CSV_HEADER = [
-  'Number',
-  'Name',
-  'Year',
-  'Theme',
-  'Pieces',
-  'Minifigs',
-  'US Retail Price',
-  'Released',
-  'US Start Date',
-  'US End Date',
-  'Brickset URL'
-]
-CSV_HEADER_LENGTH = len(CSV_HEADER)
+logging.basicConfig()
+
+config = ConfigParser.ConfigParser()
+config.read('.config')
+wanted = dict(config.items('wanted_account'))
+
+username = wanted['username']
+password = wanted['password']
+api_key = wanted['api_key']
+
+client = zeep.Client('https://brickset.com/api/v2.asmx?WSDL')
 
 
-class Set:
-  def __init__(self,
-               id,
-               number,
-               number_variant=None,
-               name=None,
-               year=None,
-               theme=None,
-               theme_group=None,
-               subtheme=None,
-               pieces=None,
-               minifigs=None,
-               released=None,
-               brickset_url=None,
-               us_retail_price=None,
-               last_updated=None,
-               us_start_date=None,
-               us_end_date=None):
-    self.id = id
-    self.number = number
-    self.number_variant = number_variant
-    self.name = name
-    self.year = year
-    self.theme = theme
-    self.theme_group = theme_group
-    self.subtheme = subtheme
-    self.pieces = pieces
-    self.minifigs = minifigs
-    self.released = released
-    self.brickset_url = brickset_url  # brickset url
-    self.us_retail_price = us_retail_price  # us retail price
-    self.last_updated = last_updated
-    self.us_start_date = us_start_date
-    self.us_end_date = us_end_date
-
-  def is_released(self):
-    return 'true' == self.released
-
-  def __str__(self):
-    return "{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(
-      self.id,
-      self.number,
-      self.number_variant,
-      self.name,
-      self.year,
-      self.theme,
-      self.theme_group,
-      self.subtheme,
-      self.pieces,
-      self.minifigs,
-      self.released,
-      self.us_retail_price,
-      self.last_updated,
-      self.us_start_date,
-      self.us_end_date,
-      self.brickset_url
-    )
-
-  def to_a(self):
-    return [
-      self.number,
-      self.name,
-      self.year,
-      self.theme,
-      self.pieces,
-      self.minifigs,
-      self.us_retail_price,
-      self.released,
-      self.us_start_date,
-      self.us_end_date,
-      self.brickset_url
-    ]
-
-
-def get_token(config):
-  data = {
-    'apiKey': config['api_key'],
-    'username': config['username'],
-    'password': config['password']
-  }
-
-  response = requests.post(LOGIN_URL, data)
-  text = response.text.encode(response.encoding)
-  doc = untangle.parse(text)
-
-  return doc.string.cdata
-
-
-def get_sets(config, token):
-  data = {
-    'apiKey': config['api_key'],
-    'userHash': token,
+def sets_params(overrides):
+  params = {
+    'apiKey': '',
+    'userHash': '',
     'query': '',
     'theme': '',
     'subtheme': '',
     'setNumber': '',
     'year': '',
     'owned': '',
-    'wanted': '1',
-    'orderBy': '',
-    'pageSize': '80',
-    'pageNumber': '1',
+    'wanted': '',
+    'orderBy': 'Number',
+    'pageSize': 50,
+    'pageNumber': 1,
     'userName': ''
   }
-
-  response = requests.post(GET_SETS_URL, data)
-  text = response.text.encode(response.encoding)
-  doc = untangle.parse(text)
-
-  if len(doc.ArrayOfSets.children) == 0:
-    return []
-
-  sets = [
-    Set(s.setID.cdata.strip(),
-        s.number.cdata.strip(),
-        s.numberVariant.cdata.strip(),
-        s.name.cdata.strip(),
-        s.year.cdata.strip(),
-        s.theme.cdata.strip(),
-        s.themeGroup.cdata.strip(),
-        s.subtheme.cdata.strip(),
-        s.pieces.cdata.strip(),
-        s.minifigs.cdata.strip(),
-        s.released.cdata.strip(),
-        s.bricksetURL.cdata.strip(),
-        s.USRetailPrice.cdata.strip(),
-        s.lastUpdated.cdata.strip())
-    for s in doc.ArrayOfSets.sets
-  ]
-
-  return sets
+  params.update(overrides)
+  return params
 
 
-def clean_date(date):
-  if date != None:
-    date = date.strip()
+token = client.service.login(apiKey=api_key, username=username, password=password)
+zeep_sets = client.service.getSets(**sets_params({'userHash': token, 'wanted': 1, 'apiKey': api_key, 'pageSize': 80}))
+sets = zeep.helpers.serialize_object(zeep_sets)
 
-  if date == None or date == '' or date == 'now':
-    cleaned = None
-  else:
-    cleaned = time.strftime("%Y%m%d", time.strptime(date, "%d %b %y"))
+key_header = OrderedDict([
+  ('number', 'Number'),
+  ('name', 'Name'),
+  ('year', 'Year'),
+  ('theme', 'Theme'),
+  ('pieces', 'Pieces'),
+  ('minifigs', 'Minifigs'),
+  ('USRetailPrice', 'US Retail Price'),
+  ('released', 'Released'),
+  ('USDateAddedToSAH', 'US Start Date'),
+  ('USDateRemovedFromSAH', 'US End Date'),
+  ('bricksetURL', 'Brickset URL')
+])
 
-  return cleaned
+sets = sorted(sets, key=lambda k: (k['number'] is None, k['number']), reverse=False)
+sets = sorted(sets, key=lambda k: (k['USDateAddedToSAH'] is None, k['USDateAddedToSAH']), reverse=False)
+sets = sorted(sets, key=lambda k: (k['released'] is None, k['released']), reverse=True)
 
+# clean up the data
+for set in sets:
+  # remove whitespace
+  for k in set.keys():
+    if isinstance(set[k], str):
+      set[k] = set[k].strip()
 
-def find_dates(tree, location):
-  id = ".//dt[text()='{}']".format(location)
+  # format the dates to YYYYMMDD
+  if set['USDateAddedToSAH']:
+    set['USDateAddedToSAH'] = datetime.strptime(set['USDateAddedToSAH'], "%Y-%m-%d").strftime("%Y%m%d")
+  if set['USDateRemovedFromSAH']:
+    set['USDateRemovedFromSAH'] = datetime.strptime(set['USDateRemovedFromSAH'], "%Y-%m-%d").strftime("%Y%m%d")
 
-  r = tree.xpath(id)
-  raw_dates = r[0].getnext().text.split('-') if r else [None, None]
+  # use true/false rather than True/False
+  set['released'] = 'true' if set['released'] else 'false'
 
-  # sometimes the dates are missing on partially released sets
-  if (len(raw_dates) != 2):
-    raw_dates = [None, None]
-
-  dates = [clean_date(d) for d in raw_dates]
-
-  return tuple(dates)
-
-
-def get_dates_from_url(url):
-  # just a little scraping
-  page = requests.get(url)
-  tree = html.fromstring(page.content)
-
-  (us_start_date, us_end_date) = find_dates(tree, 'United States')
-
-  return us_start_date, us_end_date
-
-
-def get_dates(sets):
-  for s in sets:
-    if s.is_released():
-      (s.us_start_date, s.us_end_date) = get_dates_from_url(s.brickset_url)
-    print s
-
-  return sets
-
-
-def custom_sort(a, b):
-  if a.is_released() == b.is_released():
-    if a.is_released():  # both released
-      # check for start_date, compare if both exist, None is last
-      if a.us_start_date is None and b.us_start_date is None:
-        return cmp(a.number, b.number)
-      elif (a.us_start_date is not None and b.us_start_date is not None):
-        return cmp(a.us_start_date, b.us_start_date)
-      elif a.us_start_date is None:
-        return 1
-      else:
-        return -1
-    else:  # neither released
-      return cmp(a.number, b.number)
-  elif a.is_released():
-    return -1
-  else:
-    return 1
-
-
-def output_to_csv(sets, filename=OUTPUT_CSV, uk_order_enabled=False):
-  us_ordered = map(lambda s: s.to_a(), sorted(sets, cmp=custom_sort))
-
-  if os.path.exists(filename):
-    os.remove(filename)
-
-  with open(filename, 'w') as o:
-    w = csv.writer(o, lineterminator=os.linesep)
-    w.writerows([
-      CSV_HEADER
-    ])
-    w.writerows(us_ordered)
-
-
-config = ConfigParser.ConfigParser()
-config.read('.config')
-wanted_config = dict(config.items('wanted_account'))
-retired_config = dict(config.items('retired_account'))
-
-token = get_token(wanted_config)
-sets = get_sets(wanted_config, token)
-sets = get_dates(sets)
-output_to_csv(sets, filename='wanted.csv')
-
-token = get_token(retired_config)
-sets = get_sets(retired_config, token)
-sets = get_dates(sets)
-output_to_csv(sets, filename='retired.csv')
+with open('wanted.csv', 'w') as f:
+  dict_writer = csv.DictWriter(f, fieldnames=key_header.keys(), extrasaction='ignore', lineterminator=os.linesep)
+  dict_writer.writerow(key_header)
+  dict_writer.writerows(sets)

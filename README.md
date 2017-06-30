58af64f89a28c71d6425fad184aaa3859bc398fe
Ken Schultz <schultzy51@gmail.com>
2017-06-30 12:52:37 -0500

    Catch exceptions and print json only if no errors

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 A script to assist with determining the optimal order
 
 ```
-./recent.py | jq -c '.[]'
 ./tools/brickset.py themes | jq '.[]'
+./tools/brickset.py -m 10080 -o recent | jq -c '.[]'
+./tools/brickset.py -s wanted set_order | jq -c '.[]'
 ```
--- tools/brickset.py
+++ tools/brickset.py
@@ -45,7 +45,7 @@ class Brickset:
     token = self._client.service.login(apiKey=self._api_key, username=self._username, password=self._password)
 
     if token == 'ERROR: invalid username and/or password':
-      raise 'ERROR: invalid credentials'
+      raise RuntimeError('ERROR: invalid credentials')
 
     zeep_sets = self._client.service.getSets(**self.sets_params({'userHash': token, 'wanted': 1, 'apiKey': self._api_key, 'pageSize': 80}))
     return zeep.helpers.serialize_object(zeep_sets)
@@ -135,98 +135,103 @@ def recent_custom(sets, minutes_ago_stop=0, open_web=False, unwanted_themes=None
   return sets
 
 
-api_key, username, password, unwanted_themes = config(section=args.section)
-brickset = Brickset(api_key, username, password)
 items = []
 
-if args.command == 'recent':
-  sets = brickset.recent(args.minutes_ago)
+try:
+  api_key, username, password, unwanted_themes = config(section=args.section)
+  brickset = Brickset(api_key, username, password)
 
-  if args.custom:
-    sets = recent_custom(sets, args.minutes_ago_stop, args.open_web, unwanted_themes)
+  if args.command == 'recent':
+    sets = brickset.recent(args.minutes_ago)
 
-  sets.reverse()
+    if args.custom:
+      sets = recent_custom(sets, args.minutes_ago_stop, args.open_web, unwanted_themes)
 
-  items.extend(sets)
-
-elif args.command == 'wanted':
-  sets = brickset.wanted()
-
-  if args.custom:
-    sets = wanted_custom(sets)
-
-  items.extend(sets)
-
-elif args.command == 'themes':
-  items.extend(brickset.themes())
-
-elif args.command == 'subthemes':
-  items.extend(brickset.subthemes(args.theme))
-
-elif args.command == 'years':
-  items.extend(brickset.years(args.theme))
-
-elif args.command == 'sets':
-  page_number = 1
-  page_size = 50
-
-  while True:
-    sets = brickset.sets(args.theme, page_size, page_number)
+    sets.reverse()
 
     items.extend(sets)
 
-    if len(sets) != page_size:
-      break
-    else:
-      page_number += 1
-      sleep(args.delay)
+  elif args.command == 'wanted':
+    sets = brickset.wanted()
 
-elif args.command == 'set_order':
-  sets = brickset.wanted()
-  sets = wanted_custom(sets)
-  items.extend(sets)
+    if args.custom:
+      sets = wanted_custom(sets)
 
-  key_header = OrderedDict([
-    ('number', 'Number'),
-    ('name', 'Name'),
-    ('year', 'Year'),
-    ('theme', 'Theme'),
-    ('pieces', 'Pieces'),
-    ('minifigs', 'Minifigs'),
-    ('USRetailPrice', 'US Retail Price'),
-    ('total', 'Running Total'),
-    ('released', 'Released'),
-    ('USDateAddedToSAH', 'US Start Date'),
-    ('USDateRemovedFromSAH', 'US End Date'),
-    ('lastUpdated', 'Last Updated')
-  ])
+    items.extend(sets)
 
-  total = 0
+  elif args.command == 'themes':
+    items.extend(brickset.themes())
 
-  # clean up the data
-  for wset in sets:
-    # remove whitespace
-    for k in wset.keys():
-      if isinstance(wset[k], str):
-        wset[k] = wset[k].strip()
+  elif args.command == 'subthemes':
+    items.extend(brickset.subthemes(args.theme))
 
-    # shorten the datetime
-    if wset['lastUpdated']:
-      wset['lastUpdated'] = wset['lastUpdated'].strftime("%Y-%m-%d %H:%M:%S")
+  elif args.command == 'years':
+    items.extend(brickset.years(args.theme))
 
-    # use true/false rather than True/False
-    wset['released'] = 'true' if wset['released'] else 'false'
+  elif args.command == 'sets':
+    page_number = 1
+    page_size = 50
 
-    # running total
-    if wset['released'] and wset['USDateAddedToSAH'] and wset['USRetailPrice']:
-      total = total + Decimal(wset['USRetailPrice'])
-      wset['total'] = total
+    while True:
+      sets = brickset.sets(args.theme, page_size, page_number)
 
-  with open('wanted.csv', 'w') as f:
-    dict_writer = csv.DictWriter(f, fieldnames=key_header.keys(), extrasaction='ignore', lineterminator=os.linesep)
-    dict_writer.writerow(key_header)
-    dict_writer.writerows(sets)
+      items.extend(sets)
+
+      if len(sets) != page_size:
+        break
+      else:
+        page_number += 1
+        sleep(args.delay)
+
+  elif args.command == 'set_order':
+    sets = brickset.wanted()
+    sets = wanted_custom(sets)
+    items.extend(sets)
+
+    key_header = OrderedDict([
+      ('number', 'Number'),
+      ('name', 'Name'),
+      ('year', 'Year'),
+      ('theme', 'Theme'),
+      ('pieces', 'Pieces'),
+      ('minifigs', 'Minifigs'),
+      ('USRetailPrice', 'US Retail Price'),
+      ('total', 'Running Total'),
+      ('released', 'Released'),
+      ('USDateAddedToSAH', 'US Start Date'),
+      ('USDateRemovedFromSAH', 'US End Date'),
+      ('lastUpdated', 'Last Updated')
+    ])
+
+    total = 0
+
+    # clean up the data
+    for wset in sets:
+      # remove whitespace
+      for k in wset.keys():
+        if isinstance(wset[k], str):
+          wset[k] = wset[k].strip()
+
+      # shorten the datetime
+      if wset['lastUpdated']:
+        wset['lastUpdated'] = wset['lastUpdated'].strftime("%Y-%m-%d %H:%M:%S")
+
+      # use true/false rather than True/False
+      wset['released'] = 'true' if wset['released'] else 'false'
+
+      # running total
+      if wset['released'] and wset['USDateAddedToSAH'] and wset['USRetailPrice']:
+        total = total + Decimal(wset['USRetailPrice'])
+        wset['total'] = total
+
+    with open('wanted.csv', 'w') as f:
+      dict_writer = csv.DictWriter(f, fieldnames=key_header.keys(), extrasaction='ignore', lineterminator=os.linesep)
+      dict_writer.writerow(key_header)
+      dict_writer.writerows(sets)
+  else:
+    raise RuntimeError('ERROR: Unknown Command')
+
+except Exception as e:
+  sys.exit(e)
 else:
-  sys.exit('ERROR: Unknown Command')
-
-print(json.dumps(items, default=json_serial))
+  print(json.dumps(items, default=json_serial))

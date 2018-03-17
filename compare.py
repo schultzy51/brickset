898b21a98e238115b795b9438e398d3a2f05b5d6
Ken Schultz <schultzy51@gmail.com>
2018-03-17 09:44:46 -0500

    Purchases

--- compare.py
+++ compare.py
@@ -13,7 +13,8 @@ urls = [
   'https://www.target.com/s?searchTerm=lego+{}',
   'https://www.walmart.com/search/?query=lego%20{}&cat_id=0',
   'https://www.toysrus.com/search?q=lego%20{}',
-  'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=lego+{}'
+  'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=lego+{}',
+  'https://www.ebay.com/sch/i.html?_from=R40&_nkw=lego+{}&_sacat=0&LH_BIN=1'
 ]
 
 for url in urls:

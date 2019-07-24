2a7118b990ee455b4537b175e74c040fc795829b
Ken Schultz <schultzy51@gmail.com>
2019-07-24 12:27:38 -0500

    Update and recent check

--- brickset/config.py
+++ brickset/config.py
@@ -3,7 +3,7 @@ import yaml
 
 def get_config(filename='.config.yml', section='default'):
   with open(filename, 'r') as f:
-    cfg = yaml.load(f)
+    cfg = yaml.safe_load(f)
 
   config = cfg['default']
   config.update(cfg[section])

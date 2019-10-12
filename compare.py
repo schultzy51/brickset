92c86b513cd63a43c46ebbf1c99b1f67824727e9
Ken Schultz <schultzy51@gmail.com>
2019-10-11 20:58:51 -0500

    Purge a bit

--- compare.py
+++ compare.py
@@ -9,12 +9,13 @@ parser.add_argument('set_number', help='Set Number')
 args = parser.parse_args()
 
 urls = [
+  'https://www.iwantoneofthose.com/elysium.search?search=lego+{}',
   'https://www.target.com/s?searchTerm=lego+{}',
-  'https://www.walmart.com/search/?query=lego%20{}&cat_id=0',
+  'https://www.walmart.com/search/?cat_id=0&query=lego+{}',
   'https://www.barnesandnoble.com/s/lego+{}',
-  'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=lego+{}',
-  'https://www.ebay.com/sch/i.html?_from=R40&_nkw=lego+{}&_sacat=0&LH_BIN=1',
-  'https://shop.lego.com/en-US/search?q={}'
+  'https://www.amazon.com/s?k=lego+{}',
+  'https://www.ebay.com/sch/i.html?_nkw=lego+{}',
+  'https://www.lego.com/en-US/search?q={}'
 ]
 
 for url in urls:
